467e2914b73b2ca7f7f9197d9a5295d492f2afb7
LanHao <a913538526@gmail.com>
2021-01-22 18:53:51 +0800

    第一版 测试部署

---
+++ README.md
@@ -0,0 +1,23 @@
+
+# 全球空气质量检测可视化
+
+此项目基于云开发 + [Express](https://expressjs.com/zh-cn/) 展示全球空气质量检测页面
+
+## 线上演示 & 一键部署！
+
+点击下方按钮使用 [CloudBase Framework](https://github.com/TencentCloudBase/cloudbase-framework) 可以在云端一键部署本项目到自己的云开发账号上。
+
+👇👇👇 点击一键部署自己的页面
+[![](https://main.qcloudimg.com/raw/67f5a389f1ac6f3b4d04c7256438e44f.svg)](https://console.cloud.tencent.com/tcb/env/index?action=CreateAndDeployCloudBaseProject&appUrl=https%3A%2F%2Fgithub.com%2FLanHao0%2Fcloudbase-global-aqi-visualize&branch=master)
+
+
+
+
+##参考资料
+### CloudBase Framework 相关开发配置
+
+查看 [CloudBase Framework 配置](https://github.com/TencentCloudBase/cloudbase-framework).
+
+### Express 开发文档
+
+查看 [starter](https://expressjs.com/zh-cn/starter/installing.html).
---
+++ routes/api.js
@@ -0,0 +1,70 @@
+const express = require('express');
+const router = express.Router();
+const axios = require('axios');
+
+
+router.get("/FOG_getByBounds", async function (req, res, next) {
+    const query = req.query;
+    let date = new Date().getTime();
+
+    await axios
+        .get('https://api.waqi.info/mapq/bounds/?bounds='
+            + query.leftDown + ',' + query.upRight
+            + '&inc=placeholders&k=085f8517137bddf244a523b16384f7d7e94c802a&_='
+            + date)
+        .then(response => {
+            res.json(response.data);
+        })
+});
+
+
+router.get("/FOG_getByGeo",async function (req,res,next) {
+    const query=req.query;
+    let date = new Date().getTime();
+    await axios
+        .get('https://api.waqi.info/feed/geo:'
+            + query.latitude + ';' + query.longitude
+            + '/?token=085f8517137bddf244a523b16384f7d7e94c802a&time='
+            + date)
+        .then(response => {
+            res.json(response.data);
+        })
+});
+
+router.get("/FOG_getBySearch",async function (req,res,next) {
+    const query=req.query;
+    let date = new Date().getTime();
+    await axios
+        .get('https://api.waqi.info/search/?token=085f8517137bddf244a523b16384f7d7e94c802a&time='
+            + date + '&keyword=' + query.keyword)
+        .then(response => {
+            res.json(response.data);
+        })
+});
+
+router.get("/FOG_getCountryJSON/:country",async function (req,res,next) {
+    if (req.params.country.length===2){
+        let date = new Date().getTime();
+        await axios
+            .get('https://waqi.info/rtdata/ranking/'+req.params.country+'.json?token=085f8517137bddf244a523b16384f7d7e94c802a&time=' + date)
+            .then(response => {
+                res.json(response.data);
+            })
+    }else{
+        res.json({err:"invalid country code"})
+    }
+
+});
+
+
+router.get("/allCountry",async function (req,res,next) {
+    let date = new Date().getTime();
+    await axios
+        .get('https://waqi.info/rtdata/ranking/index2.json?_' + date)
+        .then(response => {
+            res.json(response.data);
+        })
+});
+
+
+module.exports = router;
